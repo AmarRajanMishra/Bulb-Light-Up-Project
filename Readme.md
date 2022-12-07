fd9c0d557b937569d3402c8d1f2c58f12cee874c
Amar <amar1998mishra@gmail.com>
2022-12-07 15:25:01 +0530

    JavaSCript Bulb Light Up  Application

---
+++ Readme.md
@@ -0,0 +1,2 @@
+<h1>Hello This is Markdown File</h1>
+<!-- echo"Hello" -->
---
+++ script.js
@@ -0,0 +1,99 @@
+// let btn = document.getElementById('bulbBtn')
+// let bulb = document.getElementById('bulb')
+
+// btn.addEventListener('click', togleBtnFun)
+// function togleBtnFun(){
+//     if(btn.textContent.includes('On')){
+//         bulb.src = "images/ONbulb.png"
+//         btn.textContent = "Turn Off Light"
+
+//     }else{
+//         bulb.src = "images/OFFbulb.png"
+//         btn.textContent = "Turn On Light"
+
+//     }
+
+// }
+
+// var a = "This is String"
+// console.log(a.includes('amar'))
+
+
+
+
+// let btn = document.getElementById('bulbBtn')
+// let bulb = document.getElementById('bulb')
+// btn.addEventListener('click', togleBtn)
+// function togleBtn(e){
+//     if(btn.textContent.includes('On')){
+//         bulb.src = "images/ONbulb.png"
+//         btn.textContent = "Turn Off Light"
+//     }
+//     else{
+//         bulb.src = "images/OFFbulb.png"
+//         btn.textContent = "Turn On Light"
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+// let btn = document.getElementById('bulbBtn')
+// let bulb = document.getElementById('bulb')
+// let body = document.getElementById('body')
+// let text = document.getElementById('text')
+// btn.addEventListener('click', toggleBulb)
+// function toggleBulb(e){
+//     if(btn.textContent.includes('On')){
+//         bulb.src = "images/bulb-off.png"
+//         btn.textContent = "Turn Off Light"
+//         body.style.backgroundColor = "black"
+//         text.style.color = "white"
+//     }else{
+//         bulb.src = "images/bulb-on.png"
+//         btn.textContent = "Turn On Light"
+//         body.style.backgroundColor = "white"
+//         text.style.color = "black"
+
+//     }
+// }
+
+
+
+
+
+
+// Non-Primitiv Data Type
+// 1. Array  2. Object
+// var a = [2, 4, 'Amar', 56]
+// // console.log(a[3])
+// for(var i = 0; i < a.length; i++){
+//     console.log(a[i])
+// }
+
+// Object
+// var a = {
+//     Name : "Amar",
+//     age : 25,
+//     Gender:"Male"
+// }
+// console.log(a.Name)
+
+// Es6 Feature
+// var a = 10;
+// console.log("dkjnh   sgjsng", a)
+// console.log(`jfbgkjbkfjbgkjf
+// fngndljn
+// nmfkjgnjfdng"""${a}""'''''''''''''''''''`)
+
+// fetch('https://api.github.com/users')
+//   .then((response) => response.json())
+//   .then((data) => 
+//   console.log(data));
